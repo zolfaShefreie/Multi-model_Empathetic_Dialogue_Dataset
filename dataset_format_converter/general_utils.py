a9b7de0ef7d03dbd7d29d338e922f454bdec4fb2
zolfaShefreie <z.shefreie@gmail.com>
2023-06-04 11:33:14 +0330

    complete _get_result func

--- dataset_format_converter/general_utils.py
+++ dataset_format_converter/general_utils.py
@@ -1,4 +1,6 @@
 import pandas as pd
+from torch.utils.data import DataLoader
+import numpy as np
 
 from base_models_classes.empathy_kind import EmpathyKindClassifier
 from base_models_classes.exist_empathy import ExistEmpathyClassifier
@@ -10,10 +12,19 @@ class EmpathyFunctions:
     MAX_BATCH_SIZE = 50
 
     @classmethod
-    def _get_result(cls, input_data, module):
-        #TODO: complete
+    def _get_result(cls, input_data, module) -> np.ndarray:
+        """
+        define a dataloader and get results to decrease memory usage
+        :param input_data: module input data
+        :param module: can be EMPATHY_KIND_MODULE or EMPATHY_EXIST_MODULE
+        :return: result of apply module on input data
+        """
         results = list()
-        return input_data
+        dataloader = DataLoader(input_data, batch_size=cls.MAX_BATCH_SIZE)
+        iter_data_loader = iter(dataloader)
+        for data in iter_data_loader:
+            results += module(data)
+        return np.array(results)
 
     @classmethod
     def get_empathy_kind(cls,
@@ -21,10 +32,11 @@ class EmpathyFunctions:
                          utter_key_name='utterance',
                          result_key_name='empathy_kind') -> pd.DataFrame:
         """
-        :param result_key_name:
+        apply EMPATHY_KIND_MODULE
+        :param result_key_name: name of col that gonna add as result of module
         :param data:
-        :param utter_key_name:
-        :return:
+        :param utter_key_name: name of col that have utterance values
+        :return: dataframe contains result col
         """
         utterances = data[[utter_key_name]].to_numpy()
         data[result_key_name] = cls._get_result(input_data=utterances, module=cls.EMPATHY_KIND_MODULE)
@@ -37,12 +49,12 @@ class EmpathyFunctions:
                           conv_id_key_name='conv_id',
                           result_key_name='is_empathy') -> pd.DataFrame:
         """
-
+        apply EMPATHY_EXIST_MODULE
         :param data:
-        :param utter_key_name:
-        :param conv_id_key_name:
-        :param result_key_name:
-        :return:
+        :param utter_key_name: name of col that have utterance values
+        :param conv_id_key_name: name of col that have conv_id values
+        :param result_key_name: name of col that gonna add as result of module
+        :return: dataframe contains result col
         """
         conv_df = data.groupby(conv_id_key_name)[utter_key_name].apply(list).reset_index()
         conversations = conv_df[[utter_key_name]].to_numpy()
