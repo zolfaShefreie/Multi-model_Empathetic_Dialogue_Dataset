d60e02ba3b551929fcb995a884f38a2461f584fa
zolfaShefreie <z.shefreie@gmail.com>
2023-07-06 16:21:43 +0330

    adding base class for format dataset

--- dataset_format_converter/dataset_process.py
+++ dataset_format_converter/dataset_process.py
@@ -1,10 +1,102 @@
 import pandas as pd
 import json
 import ast
+from abc import ABC, abstractmethod
 
 from utils import other_utils
 from settings import RAW_DATASET_PATH
-from dataset_format_converter.general_utils import EmpathyFunctions
+from dataset_format_converter.conversation_utils import EmpathyFunctions, DialogueFunctions
+from utils.other_utils import WriterLoaderHandler
+
+
+class BaseDialogueDatasetFormatter(ABC):
+    """
+    this class show base form of stages for convert multi-model dialogue dataset to empathetic multi-model
+    dialogue dataset
+    """
+
+    # process configs
+    DATASET_NAME = str()
+    SEQ_STAGE = []
+    # some audio or video files were uploaded on youtube
+    NEED_DOWNLOAD = False
+    NEED_VIDEO_TO_AUDIO = False
+    NEED_AUDIO_SEGMENTATION = False
+
+    # metadata configs
+    CONV_ID_COL_NAME = str()
+    UTTER_ID_COL_NAME = str()
+    UTTER_COL_NAME = str()
+    SPEAKER_ID_COL_NAME = str()
+
+    def __int__(self, dataset_dir: str, save_dir: str):
+        """
+        initial of class
+        :param dataset_dir: path of dataset
+        :param save_dir: path for saving data after reformatting
+        :return:
+        """
+        self.dataset_dir = dataset_dir
+        self.save_dir = save_dir
+
+    @abstractmethod
+    def dataset_cleaner(self):
+        raise NotImplementedError
+
+    # Audio Processing Module Part
+
+    @abstractmethod
+    def file_path_manager(self):
+        raise NotImplementedError
+
+    def audio_processing(self):
+        if self.NEED_DOWNLOAD:
+            pass
+
+        if self.NEED_VIDEO_TO_AUDIO:
+            pass
+
+        if self.NEED_AUDIO_SEGMENTATION:
+            pass
+
+    # Empathetic part
+
+    @classmethod
+    def filter_two_party(cls, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        filter two part conversation based on class attribute
+        :param data: metadata with dataframe format
+        :return:
+        """
+        return DialogueFunctions.filter_two_party_dialogues(data=data,
+                                                            conv_id_key_name=cls.CONV_ID_COL_NAME,
+                                                            speaker_idx_key_name=cls.SPEAKER_ID_COL_NAME)
+
+    # Empathy Module Part
+
+    @classmethod
+    @WriterLoaderHandler.decorator(dataset_name=DATASET_NAME, process_seq=SEQ_STAGE, human_editable=True)
+    def apply_empathy_classifier(cls, data: pd.DataFrame) -> pd.DataFrame:
+        """
+        using empathy classifiers to get empathy_kind and empathy_exist
+        :param data: metadata with dataframe format
+        :return:
+        """
+        return EmpathyFunctions.add_all_empathy_cols(data=data,
+                                                     utter_key_name=cls.UTTER_COL_NAME,
+                                                     utter_id_key_name=cls.UTTER_ID_COL_NAME,
+                                                     conv_id_key_name=cls.CONV_ID_COL_NAME)
+
+    def filter_empathy_exist_conv(self, data):
+        # todo: EmpathyFunctions.filter_empathetic_conversations
+        pass
+
+    @WriterLoaderHandler.decorator(dataset_name=DATASET_NAME, process_seq=SEQ_STAGE, human_editable=True)
+    def empathetic_segmentation(self):
+        pass
+
+    def running_process(self, start_stage: str = None, stop_stage: str = None):
+        pass
 
 
 class AnnoMIDatasetFormatter:
