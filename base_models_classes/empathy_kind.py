6284189a6b772da919442e6ee882d2126da763ab
zolfaShefreie <z.shefreie@gmail.com>
2023-12-31 09:58:50 +0330

    empathy_kind llms => prompt + extract info

--- base_models_classes/empathy_kind.py
+++ base_models_classes/empathy_kind.py
@@ -3,6 +3,7 @@ import torch
 import lightning.pytorch as pl
 import torchmetrics
 import enum
+import re
 
 from utils import model_utils
 from utils import util_transforms
@@ -112,3 +113,87 @@ class EmpathyKindClassifier(model_utils.BaseDeployedModel):
             'mask': 1,
             'token_type_ids': 2
         }
+
+
+class EmpathyKindClassifierLLMs:
+    """this class uses LLMs and prompt engineering for classify utterances with EmpathyKind labels"""
+
+    SYS_PROMPT = "You are an AI expert in human communications such as conversation and you can understand empathy in" \
+                 "conversations.\nYou are tasked analyzing conversation carefully and annotate each utterance based " \
+                 "on its previous utterances as the content with three labels" \
+                 ' "empathy_seeker", "empathy_provider" and "none" and explain the reasoning behind each annotataion.'
+
+    USER_QUERY = "Empathetic conversation involves actively listening, understanding, and responding with care to " \
+                 "someone's shared thoughts, feelings, and experiences without judgment. We have three empathy " \
+                 "communication mechanisms: emotional reactions, interpretations, and explorations, each with its " \
+                 "specific intents. Emotional reactions involve expressing warmth, compassion, and concern, and " \
+                 "include intents such as Consoling, Sympathizing, Wishing, Expressing relief, Expressing care or" \
+                 " concern, Appreciating, Encouraging. Interpretations focus on understanding the conversation " \
+                 "partner's feelings and thoughts, with intents like Agreeing, Acknowledging, Sharing own " \
+                 "thoughts/opinion, Sharing or relating to own experience, Suggesting, Advising, Disapproving. " \
+                 "Explorations entail asking questions to deepen understanding and display interest, " \
+                 "aiming to elicit more information and engagement. By utilizing these mechanisms and intents, " \
+                 "individuals can create meaningful connections and support others effectively in their " \
+                 "conversations. Consider Task-oriented conversation focuses on achieving goals so it is not " \
+                 "empathetic conversation. I will give you a conversation and you will analyze it, step-by-step, " \
+                 "to classify each utterance based on its previous utterances as the content with three labels " \
+                 '"empathy_seeker", "empathy_provider" and "none". If speaker describes and shares personal ' \
+                 'experiences, feelings and thoughts, label utterance as "empathy_seeker", if speaker shows' \
+                 " the understanding of others' feelings, thoughts, or attitudes using explained empathy " \
+                 'communication mechanisms, label utterance as "empathy_provider" and Utterances that ' \
+                 "don't fall into these categories are labeled as " \
+                 '"none" For the given conversation, classify each utterance with three label "empathy_seeker", ' \
+                 '"empathy_provider" and "none" and explain the reasoning behind each classification and You will ' \
+                 'answer following this template:\n[index]: \nReason: [reason] \nLabel: [label] :\n' \
+                 "{conversation}\nAnswer: Let's think step by step to reach the right conclusion"
+
+    REGEX = r"(index:)?(\d+|\[\d+\])(\.|:)( |\n)?.*( |\n)?(((Reason|\[Reason\]): (.+)( |\n)?(Label|\[Label\]): " \
+            r"(None|none|Empathy_seeker|empathy_provider|Empathy_provider|empathy_seeker)\.?)|((Label|\[Label\]): " \
+            r"(None|none|Empathy_seeker|empathy_provider|Empathy_provider|empathy_seeker)\.?( |\n)?(Reason|\[Reason\]" \
+            r"): (.+)))"
+
+    REASON_KEY_NAME = "Reason"
+    LABEL_KEY_NAME = "Label"
+
+    @classmethod
+    def get_conversation_prompt(cls, conv_str, chat_form=False):
+        """
+        get the final prompt based on task (text or chat)
+        :param conv_str: str of conv
+        :param chat_form: is task chat completion_?
+        :return: str or a list
+        """
+        if chat_form:
+            return [
+                {"role": "system", "content": cls.SYS_PROMPT},
+                {"role": "user", "content": cls.USER_QUERY.format(conversation=conv_str)}
+            ]
+
+        else:
+            return f"{cls.SYS_PROMPT}\n{cls.USER_QUERY.format(conversation=conv_str)}"
+
+    @classmethod
+    def extract_llms_response_info(cls, response: str) -> tuple:
+        """
+        extract label and reason for each utterance
+        :param response: response of LLMs
+        :return: label list, reason list
+        """
+        labels, reasons = list(), list()
+
+        match_result = re.findall(cls.REGEX, response)
+
+        for each_match in match_result:
+            # based on regex each_match[6] and each_match[12] can contain string of reason + label
+            # each_match[x] = x'th group of regex
+            # if reason + label be in each_match[6] then each_match[12] is empty
+            # and if label + reason  are in each_match[12] then each_match[6] is empty
+            # in each_mach[6] => reason + label and reason is each_match[8] and label is each_match[11]
+            # in each_mach[12] => label + reason and label is each_match 14 and reason is each_match[17]
+            if each_match[6] or each_match[12]:
+                reasons.append(each_match[8] if each_match[6] else each_match[17])
+                labels.append(each_match[11] if each_match[6] else each_match[14])
+
+        return labels, reasons
+
+
