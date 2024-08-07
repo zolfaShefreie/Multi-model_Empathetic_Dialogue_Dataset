466be2ff5017b90f35154bfca708dac4fbe6e5ad
zolfaShefreie <z.shefreie@gmail.com>
2024-08-07 11:27:53 +0330

    edit merge to have structure for huggingface dataset

--- merge_datasets.py
+++ merge_datasets.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 import pandas as pd
 
@@ -18,7 +19,7 @@ class DatasetMerger:
     UTTER_ID_COL_NAME = "utter_id"
     UTTER_COL_NAME = "utterance"
     SPEAKER_ID_COL_NAME = "speaker"
-    FILE_PATH_COL_NAME = "file-path"
+    FILE_PATH_COL_NAME = "file_name"
 
     TRAIN_SPLIT, TEST_SPLIT, VAL_SPLIT = 0.8, 0.1, 0.1
 
@@ -75,10 +76,9 @@ class DatasetMerger:
         :return: train, val, test dataframes
         """
         conv_df = data[[cls.CONV_ID_COL_NAME]].groupby([cls.CONV_ID_COL_NAME]).count().reset_index()
-        print(conv_df.columns)
         train_conv = conv_df.sample(frac=cls.TRAIN_SPLIT, random_state=200)
         test_val_conv = conv_df.drop(train_conv.index)
-        test_conv = test_val_conv.sample(frac=cls.TEST_SPLIT, random_state=200)
+        test_conv = test_val_conv.sample(frac=cls.TEST_SPLIT/(1 - cls.TRAIN_SPLIT), random_state=200)
         val_conv = test_val_conv.drop(test_conv.index)
 
         train = pd.merge(data, train_conv[[cls.CONV_ID_COL_NAME]], on=[cls.CONV_ID_COL_NAME], how='inner')
@@ -87,7 +87,39 @@ class DatasetMerger:
 
         return train, val, test
 
+    def _file_struct_maker(self, data: pd.DataFrame, split='train') -> pd.DataFrame:
+        """
+
+        :param data:
+        :param split:
+        :return:
+        """
+        def move_file(path, conv_id, utter_idx) -> str:
+            """
+            create new path and move audio file to it
+            :param path: path of audio
+            :param conv_id: conversation id
+            :param utter_idx: utterance id
+            :return: new path
+            """
+            new_path = f"{self.SAVED_DIR}/{split}/audio/{conv_id}_{utter_idx}.wav"
+            if not os.path.exists(f"{self.SAVED_DIR}/{split}/audio/"):
+                os.makedirs(f"{self.SAVED_DIR}/{split}/audio/")
+            if not os.path.exists(new_path):
+                shutil.copy(path, new_path)
+            return f"audio/{conv_id}_{utter_idx}.wav"
+
+        data[self.FILE_PATH_COL_NAME] = data.apply(lambda x: move_file(path=x[self.FILE_PATH_COL_NAME],
+                                                                       conv_id=x[self.CONV_ID_COL_NAME],
+                                                                       utter_idx=x[self.UTTER_ID_COL_NAME]), axis=1)
+
+        return data
+
     def run(self):
+        """
+        run merger management
+        :return:
+        """
         train_dfs, test_dfs, val_dfs = list(), list(), list()
 
         for dataset_name, path_dir in self.dataset_dirs.items():
@@ -103,16 +135,19 @@ class DatasetMerger:
             os.makedirs(self.SAVED_DIR)
 
         train_df = pd.concat(train_dfs)
+        train_df = self._file_struct_maker(train_df, split='train')
         train_df = train_df.loc[:, ~train_df.columns.str.contains('^Unnamed')]
-        train_df.to_csv(f"{self.SAVED_DIR}/train.csv")
+        train_df.to_csv(f"{self.SAVED_DIR}/train/metadata.csv")
 
         test_df = pd.concat(test_dfs)
+        test_df = self._file_struct_maker(test_df, split='test')
         test_df = test_df.loc[:, ~test_df.columns.str.contains('^Unnamed')]
-        test_df.to_csv(f"{self.SAVED_DIR}/test.csv")
+        test_df.to_csv(f"{self.SAVED_DIR}/test/metadata.csv")
 
         val_df = pd.concat(val_dfs)
+        val_df = self._file_struct_maker(val_df, split='validation')
         val_df = val_df.loc[:, ~val_df.columns.str.contains('^Unnamed')]
-        val_df.to_csv(f"{self.SAVED_DIR}/validation.csv")
+        val_df.to_csv(f"{self.SAVED_DIR}/validation/metadata.csv")
 
 
 if __name__ == '__main__':
